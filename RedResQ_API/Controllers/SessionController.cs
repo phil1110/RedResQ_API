e372b9450d4d5b2161b837778706ff5d6321330f
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2023-11-07 17:04:31 +0100

    Controller importiert und Verweis angelegt

---
+++ RedResQ_API/Controllers/SessionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedResQ_API.Lib.Models;
+using RedResQ_API.Lib.Services;
+
+namespace RedResQ_API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class SessionController : ControllerBase
+	{
+		public SessionController()
+		{
+
+		}
+
+		[HttpGet]
+		public ActionResult<Session> Get(string id, string secret)
+		{
+			try
+			{
+				var output = SessionService.Login(id, secret);
+
+				if (output == null)
+				{
+					return NotFound();
+				}
+				else
+				{
+					return output;
+				}
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e.Message);
+			}
+		}
+	}
+}
