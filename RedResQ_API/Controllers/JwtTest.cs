c732ea9279cd683cdd986e39e94aea4d982de517
phil1110 <philipp.recknagel@edu.szu.at>
2023-11-26 16:24:12 +0100

    Added a temporary controller to test out JWTs

---
+++ RedResQ_API/Controllers/JwtTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RedResQ_API.Lib;
+using RedResQ_API.Lib.Models;
+using RedResQ_API.Lib.Services;
+
+namespace RedResQ_API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class JwtTest : ControllerBase
+	{
+		[HttpGet]
+		[Authorize]
+		public ActionResult<JwtClaims> Test()
+		{
+			JwtClaims claims = JwtHandler.GetClaims(this);
+
+			if (claims.Username != "string")
+			{
+				return Ok(claims);
+			}
+
+			return NotFound(claims);
+		}
+	}
+}
