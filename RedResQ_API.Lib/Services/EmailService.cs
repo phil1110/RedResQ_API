cfaef3937234a207bedba4c97cc0e35c4d8b4c8b
phil1110 <philipp.recknagel@edu.szu.at>
2023-12-04 20:02:32 +0100

    finished password reset

---
+++ RedResQ_API.Lib/Services/EmailService.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace RedResQ_API.Lib.Services
+{
+    internal static class EmailService
+    {
+        private static SmtpClient _client = new SmtpClient("smtp.gmail.com", 587)
+        {
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential("mail.redresq@gmail.com", "abkxzhwhntqevroy"),
+            EnableSsl = true
+        };
+
+        public static string SendEmail(string recipient, string content)
+        {
+            try
+            {
+                MailMessage message = new MailMessage
+                {
+                    From = new MailAddress("mail.redresq@gmail.com"),
+                    Subject = $"{DateTime.Now.ToString("dd. MMMM yyyy")}: RedResQ Password Request",
+                    Body = content,
+                    IsBodyHtml = true,
+                };
+                message.To.Add(recipient);
+
+                _client.Send(message);
+            }
+            catch (Exception e)
+            {
+                return "!ERROR: " + e.Message;
+            }
+
+            return "E-Mail was sent successfully!";
+        }
+
+        public static string GetEmail(int confirmationCode)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Good Day!");
+            sb.AppendLine("<br />");
+            sb.AppendLine("<br />");
+            sb.AppendLine("This E-Mail contains the confirmation Code to reset your password in the RedResQ-App.");
+            sb.AppendLine("<br />");
+            sb.AppendLine("<br />");
+            sb.AppendLine($"Here is your confirmation Code: {confirmationCode.ToString("D6")}");
+            sb.AppendLine("<br />");
+            sb.AppendLine("<br />");
+            sb.AppendLine("Please watch out, as the code is valid for 10 minutes only!");
+            sb.AppendLine("<br />");
+            sb.AppendLine("<br />");
+            sb.AppendLine("<br />");
+            sb.AppendLine("If it was not you who initiated the reset request, please reach out to us.");
+            sb.AppendLine("<br />");
+            sb.AppendLine("<br />");
+            sb.AppendLine("Cheers,");
+            sb.AppendLine("<br />");
+            sb.AppendLine("The RedResQ-Team");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ RedResQ_API.Lib/Services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedResQ_API.Lib.Services
+{
+    public static class UserService
+    {
+        public static bool RequestReset(string email)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            string storedProcedure = "SP_Rr_RequestPasswordReset";
+
+            parameters.Add(new SqlParameter { ParameterName = "@personEmail", SqlDbType = SqlDbType.VarChar, Value = email });
+
+            int confirmationCode = (int)SqlHandler.ExecuteQuery(storedProcedure, parameters.ToArray()).Rows[0].ItemArray[0]!;
+
+            EmailService.SendEmail(email, EmailService.GetEmail(confirmationCode));
+
+            return true;
+        }
+
+        public static bool ConfirmReset(int confirmationCode, string email, string password)
+        {
+            string hash = SessionService.HashPassword(password);
+
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            string storedProcedure = "SP_Rr_ConfirmPasswordReset";
+
+            parameters.Add(new SqlParameter { ParameterName = "@confirmationCode", SqlDbType = SqlDbType.Int, Value = confirmationCode });
+            parameters.Add(new SqlParameter { ParameterName = "@personEmail", SqlDbType = SqlDbType.VarChar, Value = email });
+            parameters.Add(new SqlParameter { ParameterName = "@hash", SqlDbType = SqlDbType.VarChar, Value = hash });
+
+            int response = (int)SqlHandler.ExecuteQuery(storedProcedure, parameters.ToArray()).Rows[0].ItemArray[0]!;
+
+            if(response == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
