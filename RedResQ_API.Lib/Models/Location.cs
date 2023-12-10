1209d0128721c9ef5d7c86b32d33bc12bcf2206d
phil1110 <philipp.recknagel@edu.szu.at>
2023-12-10 18:22:01 +0100

    Started work on the LocationService methods, both Get methods are nearly done

--- RedResQ_API.Lib/Models/Location.cs
+++ RedResQ_API.Lib/Models/Location.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,25 @@ namespace RedResQ_API.Lib.Models
 
 		public Country Country { get; private set; }
 
-		#endregion
-	}
+        #endregion
+
+        #region Methods
+
+		public static Location ConvertToLocation(DataRow row)
+		{
+            int length = row.ItemArray.Length - 1;
+
+            string countryName = Convert.ToString(row.ItemArray[length--])!;
+            long countryId = Convert.ToInt64(row.ItemArray[length--])!;
+
+			string postalCode = Convert.ToString(row.ItemArray[length--])!;
+			string city = Convert.ToString(row.ItemArray[length--])!;
+			long id = Convert.ToInt64(row.ItemArray[length--])!;
+
+            Country country = new Country(countryId, countryName);
+			return new Location(id, city, postalCode, country);
+        }
+
+        #endregion
+    }
 }
--- RedResQ_API.Lib/Services/LocationService.cs
+++ RedResQ_API.Lib/Services/LocationService.cs
@@ -1,12 +1,57 @@
-﻿using System;
+﻿using RedResQ_API.Lib.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RedResQ_API.Lib.Services
 {
-    internal class LocationService
+    public static class LocationService
     {
+        public static Location Get(long id)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            string storedProcedure = "SP_Lo_GetLocation";
+
+            parameters.Add(new SqlParameter { ParameterName = "@id", SqlDbType = SqlDbType.BigInt, Value = id });
+
+            DataTable locationTable = SqlHandler.ExecuteQuery(storedProcedure, parameters.ToArray());
+
+            if (locationTable.Rows.Count == 1)
+            {
+                return Location.ConvertToLocation(locationTable.Rows[0]);
+            }
+
+            throw new Exception("Row count was not 1!");
+        }
+
+        public static Location[] Fetch(long? id)
+        {
+            List<Location> locations = new List<Location>();
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            string storedProcedure = "SP_Lo_GetLocations";
+
+            if (id.HasValue)
+            {
+                parameters.Add(new SqlParameter { ParameterName = "@id", SqlDbType = SqlDbType.BigInt, Value = id.Value });
+            }
+
+            DataTable languageTable = SqlHandler.ExecuteQuery(storedProcedure, parameters.ToArray());
+
+            if (languageTable.Rows.Count > 0)
+            {
+                foreach (DataRow row in languageTable.Rows)
+                {
+                    locations.Add(Location.ConvertToLocation(row));
+                }
+
+                return locations.ToArray();
+            }
+
+            throw new Exception("No Locations were found!");
+        }
     }
 }
