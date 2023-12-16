d41706654d259f8c9c0b07195b5be4a54fdf2ead
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2023-12-16 12:40:46 +0100

    Moved all methods related to the password reset to their own individual Files

---
+++ RedResQ_API/Controllers/ResetController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedResQ_API.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class ResetController : ControllerBase
+    {
+        [HttpGet("request")]
+        public ActionResult RequestPasswordReset(string email)
+        {
+            try
+            {
+                bool wasSuccessful = ResetService.RequestReset(email);
+
+                if (wasSuccessful)
+                {
+                    return Ok("Successfully requested password reset!");
+                }
+                else
+                {
+                    return BadRequest("This point should be unreachable.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error! Message: " + ex.Message);
+            }
+        }
+
+        [HttpGet("confirm")]
+        public ActionResult ConfirmPasswordReset(int confirmationCode, string email, string password)
+        {
+            try
+            {
+                bool wasSuccessful = ResetService.ConfirmReset(confirmationCode, email, password);
+
+                if (wasSuccessful)
+                {
+                    return Ok("Password Request was successful!");
+                }
+                else
+                {
+                    return BadRequest("Password Request was not successful.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Error! Message: " + ex.Message);
+            }
+        }
+
+        [HttpGet("verify")]
+        public ActionResult<bool> CheckValidity(int code, string email)
+        {
+            try
+            {
+                bool isValid = ResetService.CheckValidity(code, email);
+
+                return Ok(isValid);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- RedResQ_API/Controllers/UserController.cs
+++ RedResQ_API/Controllers/UserController.cs
@@ -4,63 +4,6 @@ namespace RedResQ_API.Controllers
 {
 	public class UserController : ControllerBase
 	{
-		[HttpGet("reset/request")]
-		public ActionResult RequestPasswordReset(string email)
-		{
-			try
-			{
-				bool wasSuccessful = UserService.RequestReset(email);
-
-				if (wasSuccessful)
-				{
-					return Ok("Successfully requested password reset!");
-				}
-				else
-				{
-					return BadRequest("This point should be unreachable.");
-				}
-			}
-			catch (Exception ex)
-			{
-				return BadRequest("Error! Message: " + ex.Message);
-			}
-		}
-
-		[HttpGet("reset/confirm")]
-		public ActionResult ConfirmPasswordReset(int confirmationCode, string email,  string password)
-		{
-            try
-            {
-                bool wasSuccessful = UserService.ConfirmReset(confirmationCode, email, password);
-
-                if (wasSuccessful)
-                {
-                    return Ok("Password Request was successful!");
-                }
-                else
-                {
-                    return BadRequest("Password Request was not successful.");
-                }
-            }
-            catch (Exception ex)
-            {
-                return BadRequest("Error! Message: " + ex.Message);
-            }
-        }
-
-		[HttpGet("reset/verify")]
-		public ActionResult<bool> CheckValidity(int code, string email)
-		{
-			try
-			{
-				bool isValid = UserService.CheckValidity(code, email);
-
-				return Ok(isValid);
-            }
-			catch (Exception ex)
-			{
-				return BadRequest(ex.Message);
-			}
-		}
+		
 	}
 }
