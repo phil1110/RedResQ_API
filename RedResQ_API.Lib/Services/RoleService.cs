295aa1f6410112a0668e6ce8e1b2dacb92d317f6
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2023-12-17 12:22:24 +0100

    Added a role controller to be able to fetch roles from the database

--- RedResQ_API.Lib/Services/RoleService.cs
+++ RedResQ_API.Lib/Services/RoleService.cs
@@ -11,23 +11,60 @@ namespace RedResQ_API.Lib.Services
 {
 	public static class RoleService
 	{
-		public static Role GetRole(long id)
+        /// <summary>
+        /// Returns a role from the database based on the given identifier
+        /// </summary>
+        /// <param name="claims">The security claims from the JWT</param>
+        /// <param name="id">The identifier given to define the targeted dataset</param>
+        /// <returns>The target role</returns>
+        /// <exception cref="Exception">Is thrown, when no or more than one role was found</exception>
+        public static Role Get(JwtClaims claims, long id)
 		{
-			List<SqlParameter> parameters = new List<SqlParameter>();
-			string storedProcedure = "SP_Ro_GetRole";
-
-			parameters.Add(new SqlParameter { ParameterName = "@id", SqlDbType = SqlDbType.BigInt, Value = id });
-
-			DataTable roleTable = SqlHandler.ExecuteQuery(storedProcedure, parameters.ToArray());
-
-			if(roleTable.Rows.Count == 1)
+			if(PermissionService.IsPermitted("getRole", claims.Role))
 			{
-				return Role.ConvertToRole(roleTable.Rows[0]);
-			}
-			else
+                List<SqlParameter> parameters = new List<SqlParameter>();
+                string storedProcedure = "SP_Ro_GetRole";
+
+                parameters.Add(new SqlParameter { ParameterName = "@id", SqlDbType = SqlDbType.BigInt, Value = id });
+
+                DataTable roleTable = SqlHandler.ExecuteQuery(storedProcedure, parameters.ToArray());
+
+                if (roleTable.Rows.Count == 1)
+                {
+                    return Role.ConvertToRole(roleTable.Rows[0]);
+                }
+            }
+
+            throw new Exception("Role was not found!");
+        }
+
+        /// <summary>
+        /// Fetches all roles from the database
+        /// </summary>
+        /// <param name="claims">The security claims from the JWT</param>
+        /// <returns>An array of Role objects containing all available Roles</returns>
+        /// <exception cref="Exception">Is thrown when no roles where found in the database</exception>
+        public static Role[] Fetch(JwtClaims claims)
+        {
+			if(PermissionService.IsPermitted("fetchRoles", claims.Role))
 			{
-				throw new Exception("Role was not found!");
-			}
-		}
-	}
+                List<Role> roles = new List<Role>();
+                string storedProcedure = "SP_Ro_GetAllRoles";
+
+                DataTable roleTable = SqlHandler.ExecuteQuery(storedProcedure);
+
+                if (roleTable.Rows.Count > 0)
+                {
+                    foreach (DataRow row in roleTable.Rows)
+                    {
+                        roles.Add(Role.ConvertToRole(row));
+                    }
+
+                    return roles.ToArray();
+                }
+            }
+
+            throw new Exception("Role was not found!");
+        }
+    }
 }
---
+++ RedResQ_API/Controllers/RoleController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedResQ_API.Controllers
+{
+    [ApiController, Route("[controller]"), Authorize]
+    public class RoleController : ControllerBase
+    {
+        public ActionResult<Role> Get(long id)
+        {
+            try
+            {
+                return Ok(RoleService.Get(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public ActionResult<Role[]> Fetch()
+        {
+            try
+            {
+                return Ok(RoleService.Fetch());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
