e83997da53afa3dbb8cd9023c60cda3a850bae61
Philipp Recknagel <philipp.recknagel@edu.szu.at>
2024-01-31 22:06:21 +0100

    started work on questions but i need to stop this 6h work session now

--- RedResQ_API.Lib/Services/QuestionService.cs
+++ RedResQ_API.Lib/Services/QuestionService.cs
@@ -12,6 +12,54 @@ namespace RedResQ_API.Lib.Services
 {
     public static class QuestionService
     {
-        
+        public static bool Add(Question question)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            DataTable answers = new DataTable();
+            string storedProcedure = "SP_Qn_AddQuestion";
+
+            answers.Columns.Add("QuizID", typeof(long));
+            answers.Columns.Add("QuestionID", typeof(long));
+            answers.Columns.Add("ID", typeof(long));
+            answers.Columns.Add("Text", typeof(string));
+            answers.Columns.Add("IsTrue", typeof(bool));
+
+            foreach (var a in question.Answers)
+            {
+                answers.Rows.Add(a.QuizId, a.QuestionId, a.Id, a.Text, a.IsTrue);
+            }
+
+            parameters.Add(new SqlParameter { ParameterName = "@quizId", SqlDbType = SqlDbType.BigInt, Value = question.QuizId });
+            parameters.Add(new SqlParameter { ParameterName = "@id", SqlDbType = SqlDbType.BigInt, Value = question.Id });
+            parameters.Add(new SqlParameter { ParameterName = "@text", SqlDbType = SqlDbType.VarChar, Value = question.Text });
+            parameters.Add(new SqlParameter { ParameterName = "@answers", SqlDbType = SqlDbType.Structured, Value = answers });
+
+            int rowsAffected = SqlHandler.ExecuteNonQuery(storedProcedure, parameters.ToArray());
+
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool Delete(long quizId, long id)
+        {
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            string storedProcedure = "SP_Qn_DeleteQuestion";
+
+            parameters.Add(new SqlParameter { ParameterName = "@quizId", SqlDbType = SqlDbType.BigInt, Value = quizId });
+            parameters.Add(new SqlParameter { ParameterName = "@id", SqlDbType = SqlDbType.BigInt, Value = id });
+
+            int rowsAffected = SqlHandler.ExecuteNonQuery(storedProcedure, parameters.ToArray());
+
+            if (rowsAffected > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
---
+++ RedResQ_API/Controllers/QuestionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedResQ_API.Controllers
+{
+    [ApiController, Route("[controller]"), Authorize]
+    public class QuestionController
+    {
+        [HttpGet("get")]
+        public ActionResult<Question> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
